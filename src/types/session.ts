e356c74a2bc7810e56a254241b856d955f7570eb
Salino3 <flaviobcn012@gmail.com>
2026-08-22 10:16:34 +0200

    update interfaces

--- src/types/session.ts
+++ src/types/session.ts
@@ -1,9 +1,22 @@
 import { Context, Filter } from "grammy";
 
+export type WizardStep =
+  | "AWAITING_TITLE"
+  | "AWAITING_DESCRIPTION"
+  | "AWAITING_LOCATION"
+  | "AWAITING_PRIORITY"
+  | "AWAITING_DATE"
+  | "AWAITING_DURATION";
+
+export type PriorityType = "low" | "medium" | "high";
+
 export interface UserSessionProps {
   step: "AWAITING_TITLE" | "AWAITING_PRIORITY" | "AWAITING_DATE";
   title?: string;
-  priority?: "low" | "medium" | "high";
+  description?: string;
+  location?: string;
+  priority?: PriorityType;
+  durationMinutes?: number;
 }
 
 export type TextContextType = Filter<Context, "message:text">;
